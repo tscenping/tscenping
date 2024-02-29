4c9ed317e5b93f6d035f9cc0d836c3e4f1ca2bf6
HyeongwooIM <hyeongwoo1279@gmail.com>
2024-02-29 11:32:59 +0900

    [Feat] Main페이지 랭킹 추가 #2

--- src/components/Main/Game/GameTypeSelector.tsx
+++ src/components/Main/Game/GameTypeSelector.tsx
@@ -1,56 +1,60 @@
+export enum GameType {
+  NORMAL = 'NORMAL',
+  LADDER = 'LADDER',
+}
+
 interface GameTypeSelectorProps {
-  gameType: "NORMAL" | "LADDER";
-  setGameType: (gameType: "NORMAL" | "LADDER") => void;
+  gameType: GameType;
+  setGameType: (gameType: GameType) => void;
 }
 
 export default function GameTypeSelector(props: GameTypeSelectorProps) {
-  const selectedLabel = "text-black flex-1 z-10 text-center";
-  const unselectedLabel = "text-white flex-1 text-center cursor-pointer w-full h-full flex justify-center items-center hover:scale-110 transition-transform duration-150";
+  const selectedLabel = 'text-black flex-1 z-10 text-center';
+  const unselectedLabel =
+    'text-white flex-1 text-center cursor-pointer w-full h-full flex justify-center items-center hover:scale-110 transition-transform duration-150';
   const selectedRadio = `absolute w-1/2 h-full bg-white rounded-lg transition-transform duration-300 ease-in-out scale-110 ${
-    props.gameType === "LADDER" ? "translate-x-0" : "translate-x-full"
+    props.gameType === GameType.LADDER ? 'translate-x-0' : 'translate-x-full'
   }`;
 
   return (
-    <>
-      <div className="flex flex-1 justify-center items-center w-2/5 px-5 ">
-        <input
-          type="radio"
-          value="LADDER"
-          name="gameType"
-          id="LADDER"
-          checked={props.gameType === "LADDER"}
-          className="hidden"
-          onChange={() => props.setGameType("LADDER")}
-        />
-        <input
-          type="radio"
-          value="NORMAL"
-          name="gameType"
-          id="NORMAL"
-          className="hidden"
-          checked={props.gameType === "NORMAL"}
-          onChange={() => props.setGameType("NORMAL")}
-        />
-        <div className="relative flex flex-1 bg-buttonBg h-3/5 rounded-lg flex-basis max-w-sm min-w-[200px] items-center ">
-          <div className={selectedRadio}></div>
-          <label
-            htmlFor="LADDER"
-            className={
-              props.gameType === "LADDER" ? selectedLabel : unselectedLabel
-            }
-          >
-            Ladder
-          </label>
-          <label
-            htmlFor="NORMAL"
-            className={
-              props.gameType === "NORMAL" ? selectedLabel : unselectedLabel
-            }
-          >
-            Normal
-          </label>
-        </div>
+    <div className="flex items-center justify-center flex-1 w-2/5 px-5 ">
+      <input
+        type="radio"
+        value="LADDER"
+        name="gameType"
+        id="LADDER"
+        checked={props.gameType === GameType.LADDER}
+        className="hidden"
+        onChange={() => props.setGameType(GameType.LADDER)}
+      />
+      <input
+        type="radio"
+        value="NORMAL"
+        name="gameType"
+        id="NORMAL"
+        className="hidden"
+        checked={props.gameType === GameType.NORMAL}
+        onChange={() => props.setGameType(GameType.NORMAL)}
+      />
+      <div className="relative flex flex-1 bg-buttonBg h-3/5 rounded-lg flex-basis max-w-sm min-w-[200px] items-center ">
+        <div className={selectedRadio}></div>
+        <label
+          htmlFor="LADDER"
+          className={
+            props.gameType === GameType.LADDER ? selectedLabel : unselectedLabel
+          }
+        >
+          Ladder
+        </label>
+        <label
+          htmlFor="NORMAL"
+          className={
+            props.gameType === GameType.NORMAL ? selectedLabel : unselectedLabel
+          }
+        >
+          Normal
+        </label>
       </div>
-    </>
+    </div>
   );
 }
--- src/components/Main/Game/MatchBtn.tsx
+++ src/components/Main/Game/MatchBtn.tsx
@@ -1,33 +1,28 @@
-import { useState } from "react";
-import GameTypeSelector from "./GameTypeSelector";
+import {useState} from "react";
+import GameTypeSelector, {GameType} from "./GameTypeSelector";
 import GameStartBtn from "./GameStartBtn";
 import SpecialCheck from "../../../img/Main/SpecialCheck.svg";
 import SpecialUncheck from "../../../img/Main/SpecialUncheck.svg";
 
 
 export default function MatchBtn() {
-  const [gameType, setGameType] = useState<"NORMAL" | "LADDER">("NORMAL");
+  const [gameType, setGameType] = useState<GameType>(GameType.NORMAL);
   const [isSpecial, setIsSpecial] = useState<boolean>(false);
 
-  const changeSpecial = () =>{
+    const changeSpecial = () =>{
       setIsSpecial((prev) => !prev)
     }
 
   return (
     <div className="flex w-full flex-col h-1/6 justify-center relative items-center gap-1 max-w-5xl max-h-40 ">
-      <GameTypeSelector gameType={gameType} setGameType={setGameType} />
-        <div className="flex gap-2 h-2/5 w-2/5 justify-around">
-        {/*{gameType === "LADDER" ? */}
-            <button className={`absolute rounded-md bg-customGreen w-2/5 h-2/5 text-black min-w-[220px] font-bold hover:scale-105 ${gameType === "LADDER" ? "opacity-100" : "opacity-0"} transition-opacity duration-500 ease-in-out`}> Game Start </button>
-        {/*:*/}
-            <GameStartBtn gameType={gameType}/>
-
-        {/*}*/}
-        </div>
-        <label htmlFor="SpecialMode" className={`flex gap-3 hover:scale-130 cursor-pointer ${gameType === "NORMAL" ? "opacity-100" : "opacity-0"} transition-opacity duration-500 ease-in-out` }>
+        <GameTypeSelector gameType={gameType} setGameType={setGameType} />
+        <GameStartBtn gameType={gameType} />
+        <div className="h-1/5">
+        <label htmlFor="SpecialMode" className={`flex gap-3 hover:scale-130 cursor-pointer ${gameType === GameType.NORMAL ? "opacity-100" : "opacity-0 hidden"} transition-opacity duration-500 ease-in-out` }>
         <input type={"checkbox"} id="SpecialMode" className={`scale-150 mr-3 ${gameType === "NORMAL" ? "opacity-100" : "opacity-0"} transition-opacity duration-500 ease-in-out hidden` } checked={isSpecial} onChange={changeSpecial}/>
-            <img src={isSpecial ? SpecialCheck : SpecialUncheck} className="scale-110 transition-transform duration-500 ease-in-out  cursor-pointer"/>
+            <img src={isSpecial ? SpecialCheck : SpecialUncheck} className="scale-110 transition-transform duration-500 ease-in-out  cursor-pointer" alt={"체크박스"}/>
             Special Mode</label>
+        </div>
     </div>
   );
 }
