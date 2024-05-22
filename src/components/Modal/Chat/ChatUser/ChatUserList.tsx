f1d75b24b20a9c7670dab3db1e0de729e4aff5f8
exceed96 <exceed96@naver.com>
2024-05-22 16:04:37 +0900

    [Fix] chat user list style edit #1

--- src/components/Modal/Chat/ChatUser/ChatUserList.tsx
+++ src/components/Modal/Chat/ChatUser/ChatUserList.tsx
@@ -20,8 +20,8 @@ const ChatUserList = (props: ChatUsersInfoTypes): JSX.Element => {
         <img
           src={props.avatar ? props.avatar : defaultProfile}
           alt="profile"
-          // className="object-cover w-10 h-10 mr-3 rounded-full sm:w-12 sm:h-12 lg:w-13 lg:h-13 xl:w-16 xl:h-16"
-          className="object-cover mr-3 rounded-full h-full w-1/4 sm:w-1/3"
+          className="object-cover mr-3 rounded-full w-8 h-8 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-13 lg:h-13 xl:w-16 xl:h-16"
+          // className="object-cover mr-3 rounded-full h-full w-1/4 sm:w-1/3"
         />
         <span className="font-[Pretendard] text-sm sm:text-base lg:text-lg xl:text-xl">
           {props.nickname}
@@ -31,7 +31,7 @@ const ChatUserList = (props: ChatUsersInfoTypes): JSX.Element => {
         inChatInfo.channelType !== "DM" && (
           <img
             src={userSetting}
-            className="cursor-pointer w-6 sm:w-7 md:w-8"
+            className="cursor-pointer w-4 xxs:w-5 xs:w-6 sm:w-7 md:w-8"
             alt="img"
             onClick={() => {
               if (dropDownType === "NONE") setDropDownType("CHAT");
